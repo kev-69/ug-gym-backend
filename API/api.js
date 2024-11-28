83d3f2cb1d5b9182b66688103d5f78d695f37d74
kev-69 <bismarkobuobi19@gmail.com>
2024-11-28 19:16:10 +0000

    added hosted backend link

--- API/api.js
+++ API/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:4000/api/users"
+    baseURL: "https://ug-gym-backend.onrender.com/api/users"
 });
 
 API.interceptors.request.use((config) => {
