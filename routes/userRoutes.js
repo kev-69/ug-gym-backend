3b33a4a181fbb60a1f715f8dac28dadf47ef5b20
kev-69 <bismarkobuobi19@gmail.com>
2024-11-28 11:48:16 +0000

    added a more understandablem comment

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,8 +9,9 @@ const router = express.Router();
 
 // Routes
 router.post("/register", registerUser);
-router.post("/login", loginUniversityUser);
-router.post("/login", loginPublicUser);
+// Separate the routes for university and public user login
+router.post("/login/university", loginUniversityUser); // For university users (student or staff)
+router.post("/login/public", loginPublicUser); // For public users
 router.get("/profile", getUserProfile);
 
 module.exports = router;
