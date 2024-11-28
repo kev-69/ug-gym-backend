472766f0d4828beb1fcee9f2ed0edc74635961b0
kev-69 <bismarkobuobi19@gmail.com>
2024-11-28 18:03:48 +0000

    changed port number

--- API/api.js
+++ API/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:3000/api/users"
+    baseURL: "http://localhost:4000/api/users"
 });
 
 API.interceptors.request.use((config) => {
