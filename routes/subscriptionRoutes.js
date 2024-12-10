3ffa78f3e04331ee4298495794f3b449d3f04695
kev-69 <bismarkobuobi19@gmail.com>
2024-12-10 17:24:31 +0000

    subcription routes

---
+++ routes/subscriptionRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const {
+    addPendingSubscription,
+    activateSubscription,
+    getSubscriptionDetails,
+} = require("../controllers/subscriptionController");
+
+// Add a pending subscription
+router.post("/add-pending", addPendingSubscription);
+
+// Activate a subscription after payment
+router.post("/activate", activateSubscription);
+
+// Get subscription details
+router.get("/:userId", getSubscriptionDetails);
+
+module.exports = router;
