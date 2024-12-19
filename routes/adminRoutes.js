6bdd1433a611f48e067a28ef52a68f05751feafc
kev-69 <bismarkobuobi19@gmail.com>
2024-12-19 07:49:27 +0000

    route to get all users for admins

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { registerAdmin, loginAdmin } = require("../controllers/adminController");
+const { registerAdmin, loginAdmin, getAllUsers } = require("../controllers/adminController");
+const { protect, verifyAdmin } = require("../middlewares/authMiddleware");
+
 
 // Routes
 // route to register admin user
 router.post("/register", registerAdmin);
 // route to login admin user
 router.post("/login", loginAdmin);
+// route to get all users
+router.get("/all-users", protect, verifyAdmin, getAllUsers)
 
 module.exports = router;
