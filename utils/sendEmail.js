20dc2e11591956317febf0325f75937ddf0c414b
kev-69 <bismarkobuobi19@gmail.com>
2025-01-06 15:43:28 +0000

    replaced the email with the mailersend username rather

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -4,6 +4,7 @@ const sendEmail = async (email, subject, message) => {
     const transporter = nodemailer.createTransport({
         host: process.env.MAILSEND_SMTP_HOST, // Your MailSend SMTP host
         port: process.env.MAILSEND_SMTP_PORT, // Your MailSend SMTP port
+        secure: false,
         auth: {
             user: process.env.MAILSEND_SMTP_LOGIN, // Your MailSend SMTP login
             pass: process.env.MAILSEND_SMTP_PASSWORD // Your MailSend SMTP password
@@ -11,7 +12,7 @@ const sendEmail = async (email, subject, message) => {
     });
     
     const mailOptions = {
-        from: process.env.MAILSEND_FROM_EMAIL, // Your verified MailSend email
+        from: process.env.MAILSEND_SMTP_LOGIN, // Your verified MailSend email
         to: email,
         subject: subject,
         text: message
