8d40939d2641d1dc45a1d50c4faeb3ab894c1e64
kev-69 <bismarkobuobi19@gmail.com>
2025-01-15 17:07:34 +0000

    added local host url as comment

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,18 +15,25 @@ const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
 
 const { protect } = require("../middlewares/authMiddleware")
-// console.log(typeof protect); // Should log "function"
 
-// Routes
+// ROUTES
+// http://localhost:4000/api/users/register --- POST
 router.post("/register", registerUser);
-// Separate the routes for university and public user login
+// http://localhost:4000/api/users/login/university --- POST
 router.post("/login/university", loginUniversityUser); // For university users (student or staff)
+// http://localhost:4000/api/users/login/public --- POST
 router.post("/login/public", loginPublicUser); // For public users
+// http://localhost:4000/api/users/profile --- GET
 router.get("/profile", protect, getUserProfile);
+// http://localhost:4000/api/users/profile --- PATCH
 router.patch('/profile', protect, upload.single('passportPhoto'), updatePassportPhoto);
+// http://localhost:4000/api/users/generate-id --- GET
 router.get("/generate-id", generateId)
+// http://localhost:4000/api/users/send-otp --- POST
 router.post("/send-otp", sendOtp);
+// http://localhost:4000/api/users/verify-otp --- POST
 router.post("/verify-otp", verifyOtp);
+// http://localhost:4000/api/users/reset-password --- POST
 router.post("/reset-password", resetPassword);
 
 module.exports = router;
